8c235775796305f03c80ed7c6b38aee2b93d2fd1
Alphonse <cashmoov@cashmoov.net>
2025-12-02 11:17:59 +0000

    modification to the router version 2

--- src/main.py
+++ src/main.py
@@ -18,14 +18,14 @@ app = FastAPI(title="CashMoov API", version="1.0.0")
 Base.metadata.create_all(bind=engine)
 
 # Include all routers
-app.include_router(transaction_router)
-app.include_router(country_router)
+app.include_router(auth_router)  # If you have auth
 app.include_router(user_router)
+app.include_router(company_router)  # Add this line
+app.include_router(country_router)
 app.include_router(balance_router)
 app.include_router(fee_router)
 app.include_router(procurement_router)
-app.include_router(company_router)  # Add this line
-app.include_router(auth_router)  # If you have auth
+app.include_router(transaction_router)
 app.include_router(email_router)  # If you have auth
 
 
